4b02292a87f7ea7c68544038b4fb6c3f5fd02b5e
Quynh Nguyen <tom@coh.dev>
2022-12-15 01:44:26 +0700

    remove sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,4 @@
 module.exports = ({ env }) => ({
-    // ...
-    email: {
-      config: {
-        provider: "sendgrid",
-        providerOptions: {
-          apiKey: "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",
-        },
-        settings: {
-          defaultFrom: "contact@echomedi.com",
-          defaultReplyTo: "contact@echomedi.com",
-        },
-      },
-    },
     'users-permissions': {
         config: {
           jwt: {
