6e71b8e69481f825de6e56283ac5d46d75317baa
Quynh Nguyen <tom@coh.dev>
2023-01-07 08:41:54 +0700

    u

---
+++ src/api/index-image/controllers/index-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * index-image controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::index-image.index-image');
---
+++ src/api/index-image/routes/index-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * index-image router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::index-image.index-image');
